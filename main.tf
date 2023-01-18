2a653b685667cad8ee9bf791d968473e8d81318c
Sonali Arora <sonaliarora2012@icloud.com>
2023-01-18 20:47:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,20 @@
-resource "aws_iam_role" "example" {
-  name               = "example"
-  path = "/"
-
-  assume_role_policy = <<EOF
-    {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-      }
-    ]
-    }
-    EOF
+variable "cluster_name" {
+  default = "example"
+  type    = string
 }
 
 resource "aws_eks_cluster" "example" {
-  name     = "example-cluster"
-  role_arn = aws_iam_role.example.arn
+  depends_on = [aws_cloudwatch_log_group.example]
 
-  vpc_config {
-    endpoint_private_access = true
-    endpoint_public_access  = false
-  }
+  enabled_cluster_log_types = ["api", "audit"]
+  name                      = var.cluster_name
 
   tags = {
-    Owner = "stackguardian"
+    "Owner" = "stackguardian"
   }
+}
 
-  outpost_config {
-    control_plane_instance_type = "m5d.large"
-    outpost_arns                = [data.aws_outposts_outpost.example.arn]
-  }
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/eks/${var.cluster_name}/cluster"
+  retention_in_days = 7
 }
