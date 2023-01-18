334d346881da31427c70a25c584f837aa7bfb60c
Sonali Arora <sonaliarora2012@icloud.com>
2023-01-18 20:42:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
 resource "aws_iam_role" "example" {
   name               = "example"
+  path = "/"
+
+  assume_role_policy = <<EOF
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+      }
+    ]
+    }
+    EOF
 }
 
 resource "aws_eks_cluster" "example" {
